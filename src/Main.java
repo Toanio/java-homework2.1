34d16194b47f1b00681fa6582d611d30477ad65f
Чан Тоан <c.toan@corp.mail.ru>
2020-04-30 18:47:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int customer_balance=2_000_000_000;
+        int transfer_amount=500_000_000;
+        int account_balance=customer_balance+transfer_amount;
+
+        System.out.println(account_balance);
+
+    }
+}
